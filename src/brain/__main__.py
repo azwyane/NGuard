1e9501b843b67aaab8a1d1ce0a8e6fd507d60e29
azwyane <debianbyte@gmail.com>
2022-01-22 17:15:21 +0545

    Nearly Done:model interface

--- src/brain/__main__.py
+++ src/brain/__main__.py
@@ -1,32 +1,68 @@
 import os
 from time import sleep
 import glob
+import pathlib
 import pandas as pd
 import pickle
 from . import classifier
 
 
-cwd = os.getcwd()
-DATA_PATH = os.path.join(cwd,"cicflowmeter/data/daily")
-MODEL_PATH = os.path.join(cwd,"brain/models")
-last_read_index = 0
-with open(MODEL_PATH +'/binary_rf_model','rb') as f:
-    binaryclass_model = pickle.load(f)
 
-with open(MODEL_PATH +'/binary_rf_model','rb') as f:
-    multiclass_model = pickle.load(f)
 
-while True:
-    if file:=glob.glob(DATA_PATH+"/*Flow.csv"): #look for file 
-        print("Found")
-        df = pd.read_csv(file[0])
-        cleaned_df = classifier.cleanser(df)
-        classifier.binaryclassifier(cleaned_df,binaryclass_model)
-        classifier.multiclassclassifier(cleaned_df,multiclass_model)
-    else:
-        print("not found")
-        sleep(5)
-    # if exists: read from index last read till EOL, get index of last item and update last_read_index
-    #if not sleep for 5 sec
+pid = os.fork()
+
+if pid > 0:
+   
+    cwd = os.getcwd()
+    DATA_PATH = os.path.join(cwd,"cicflowmeter/data/daily")
+    MODEL_PATH = os.path.join(cwd,"brain/models")
+    s_read_index = 0
+    print("Parent process:Binary classifier","pid:",os.getpid())
+    with open(MODEL_PATH +'/binary_rf_model','rb') as f:
+        binaryclass_model = pickle.load(f)
+    
+    
+    while True:
+        if tfile:=glob.glob(DATA_PATH +"/*Flow.csv"): 
+            df = pd.read_csv(tfile[0])
+            s_read_index = 0
+            temp = classifier.binaryclassifier(s_read_index,df.copy(),binaryclass_model)
+            s_read_index += temp
+   
+        else:
+            print("not found")
+            sleep(5)
+        
+
+
+else:
+    
+    cwd = os.getcwd()
+    DATA_PATH = os.path.join(cwd,"cicflowmeter/data/daily")
+    PR_DATA_PATH = os.path.join(cwd,"server/predicted_block.csv")
+    MODEL_PATH = os.path.join(cwd,"brain/models")
+    start_at_index = 0
+    print("Child process:Multi Class classifier","pid:",os.getpid())
+    with open(MODEL_PATH +'/multi_rf_model','rb') as modelf:
+        multiclass_model = pickle.load(modelf)
+
+    while True:
+
+        if pathlib.Path(PR_DATA_PATH).is_file():
+            try:
+                predicted_block = pd.read_csv(PR_DATA_PATH)
+                total_read = classifier.multiclassclassifier(start_at_index,predicted_block,multiclass_model)
+                start_at_index += total_read
+            except Exception as e:
+                sleep(20)
+
+           
+     
+   
+    
+
+
+ 
+
 
 
--- src/brain/classifier.py
+++ src/brain/classifier.py
@@ -1,32 +1,74 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import f1_score
-from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import scale
 import pathlib
 import os
+import hashlib
+from sklearn.preprocessing import LabelEncoder
+
 
 CWD = os.getcwd()
-
-def cleanser(df):
-    df = df[[' Bwd Packet Length Min', ' Subflow Fwd Bytes','Total Length of Fwd Packets',' Fwd Packet Length Mean']]
-    df = scale(df,copy=False)
-    return df
+SAVE_TO = CWD + '/server'
 
 
-def binaryclassifier(df,binaryclass_model):
-    if 
-    y_predicted = binaryclass_model.predict(df)
+def bcleanser(s_read_index,df):
+    bdf = df.copy()
+    bdf = bdf[['Bwd Pkt Len Min', 'Subflow Fwd Byts','TotLen Fwd Pkts','Fwd Pkt Len Mean']]
+    bdf = bdf.iloc[s_read_index:]
+    bdf = scale(np.nan_to_num(bdf.values))
+    return bdf
 
-def multiclassclassifier(df,multiclass_model):
-    # y_predicted = model.predict(df)
-    pass
+def mcleanser(start_at_index, predicted_block):
+    mdf = predicted_block.copy()
+    mdf = mdf[[
+       'Flow Duration', 'TotLen Fwd Pkts', 'Fwd Pkt Len Mean', 'Bwd Pkt Len Min',
+       'Bwd Pkt Len Std', 'Flow IAT Mean', 'Flow IAT Std', 'Fwd IAT Mean', 
+       'Fwd IAT Min', 'Bwd IAT Mean', 'Bwd Pkts/s','PSH Flag Cnt',
+       'Pkt Size Avg', 'Subflow Fwd Byts', 'Init Fwd Win Byts', 'Init Bwd Win Byts',
+       'Active Mean', 'Active Min'
+    ]]
+    mdf = mdf.iloc[start_at_index:]
+    mdf = np.nan_to_num(mdf.values)
+    return mdf
+    
+
+def binaryclassifier(s_read_index,df,binaryclass_model):
+    x_clean = bcleanser(s_read_index,df)
+    y_predicted = binaryclass_model.predict(x_clean)
+    y_index = np.where(y_predicted == 1)[0]
+    if  y_index.any():    
+        ndf = df.iloc[y_index].copy()
+        ndf.rename(columns = {'Src IP':'sip', 'Src Port':'sport', 'Dst IP':'dip', 'Dst Port':'dport', 'Protocol':'proto'},inplace =True)
+        ndf['hash_val'] = ndf.apply(
+            lambda row: hashlib.md5((str(row['sip'])+str(row['sport'])+str(row['dip'])+str(row['dport'])+str(row['proto'])).encode('utf-8')).hexdigest(),
+            axis=1)
+        if pathlib.Path(SAVE_TO + '/predicted_block.csv').is_file():    
+            ndf.to_csv(SAVE_TO + '/predicted_block.csv',mode='a',header=False)
+        else:
+            ndf.to_csv(SAVE_TO + '/predicted_block.csv',header=True)
+        
+    return df.shape[0]   
+
+
+def multiclassclassifier(start_at_index,predicted_block,multiclass_model):
+    x_cleaned = mcleanser(start_at_index, predicted_block)
+    y_predictedm = multiclass_model.predict(x_cleaned)
+    new_df = pd.DataFrame({'Intrusion':y_predictedm})
+    new_df = pd.concat([predicted_block.copy(),new_df],axis=1)
+    if pathlib.Path(SAVE_TO + '/final_predicted_intrusion.csv').is_file():    
+        new_df.to_csv(SAVE_TO + '/final_predicted_intrusion.csv',mode='a',header=False)
+    else:
+        new_df.to_csv(SAVE_TO  + '/final_predicted_intrusion_class.csv',header=True)
+  
+    return new_df.shape[0]
 
 
 
- 
+
+
+
+
 
 
 
