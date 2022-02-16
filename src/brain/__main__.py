a43e83cd347cada9707db3f536ac28dacdd0d70a
azwyane <debianbyte@gmail.com>
2022-02-16 13:44:36 +0545

    modification of brain script

--- src/brain/__main__.py
+++ src/brain/__main__.py
@@ -1,69 +1,123 @@
-import os
-from time import sleep
-import glob
-import pathlib
+
 import pandas as pd
-import pickle
-from . import classifier
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 from joblib import dump,load
-from sklearn.ensemble import VotingClassifier
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import os
+import configparser
+import sys
+
+
+cparser = configparser.RawConfigParser()
+cwd = os.getcwd()
+
+if not cparser.read(os.path.join(cwd,"brain.config")):
+    raise Exception("Brain needs a config file: brain.config")
+    sys.exit()
+
+output_dir = cparser.get('brain','output_dir')
+if not os.path.isdir(f'{cwd}/{output_dir}'):
+    os.makedirs(f'{cwd}/{output_dir}')
+
+output_dir = f'{cwd}/{output_dir}'
+
+DATA_PATH = os.path.join(cwd,"cicflowmeter/data/daily/")
+MODEL_PATH = os.path.join(cwd,"brain/models/")
+
+
+binary_clf_path = f'{MODEL_PATH}binary/'
+multi_clf_path = f'{MODEL_PATH}multiclass/'
+
+
+#for binary classification
+bpca = load(f'{binary_clf_path}bpca.joblib')
+bscaler = load(f'{binary_clf_path}bscaler.joblib')
+bclf = load(f'{binary_clf_path}binary.joblib')
+
+
+#for multi binary classfication
+dos_clf = load(f'{multi_clf_path}dos')
+ddos_clf = load(f'{multi_clf_path}ddos')
+portscan_clf = load(f'{multi_clf_path}portscan')
+patator_clf = load(f'{multi_clf_path}patator')
+web_clf= load(f'{multi_clf_path}web')
+
+clf_s = [dos_clf,ddos_clf,portscan_clf,patator_clf,web_clf]
+
+
+#for multi binary classfication with PCA
+dos = load(f'{multi_clf_path}withpca/dos')
+ddos = load(f'{multi_clf_path}withpca/ddos')
+portscan = load(f'{multi_clf_path}withpca/portscan')
+patator = load(f'{multi_clf_path}withpca/patator')
+web = load(f'{multi_clf_path}withpca/web')
+
+
+dosscl = load(f'{multi_clf_path}withpca/dosscaler')
+ddosscl = load(f'{multi_clf_path}withpca/ddosscaler')
+portscanscl = load(f'{multi_clf_path}withpca/portscanscaler')
+patatorscl = load(f'{multi_clf_path}withpca/patatorscaler')
+webscl = load(f'{multi_clf_path}withpca/webscaler')
+
+dospca = load(f'{multi_clf_path}withpca/dospca')
+ddospca = load(f'{multi_clf_path}withpca/ddospca')
+portscanpca = load(f'{multi_clf_path}withpca/portscanpca')
+patatorpca = load(f'{multi_clf_path}withpca/patatorpca')
+webpca = load(f'{multi_clf_path}withpca/webpca')
+
+clfs = [dos,ddos,portscan,patator,web]
+scls = [dosscl,ddosscl,portscanscl,patatorscl,webscl]
+pcas = [dospca,ddospca,portscanpca,patatorpca,webpca]
+attack_names = ['dos','ddos','portscan','patator','web']
+
+
+features = {
+    'dos_feature':[ 'Bwd Pkts/s', 'Pkt Len Min', 'Bwd Pkt Len Std', 'FIN Flag Cnt',
+                    'Fwd IAT Mean', 'Init Fwd Win Byts', 'Fwd Pkt Len Max', 'Fwd PSH Flags',
+                    'SYN Flag Cnt', 'Fwd IAT Min', 'Init Bwd Win Byts', 'Flow IAT Min',
+                    'Pkt Size Avg', 'Fwd Seg Size Min', 'Fwd Header Len'],
+    'ddos_feature':[ "Fwd Pkt Len Max","Subflow Fwd Byts","TotLen Fwd Pkts"],
+    'portscan_feature':[ 'Subflow Fwd Byts','TotLen Fwd Pkts','Fwd Pkt Len Max','Pkt Size Avg'],
+    'patator_feature':[ "Init Fwd Win Byts", "Fwd Seg Size Min", "Pkt Len Std",
+                         "Init Bwd Win Byts","Flow IAT Min","Pkt Size Avg"],
+    'web_feature':[ "Init Bwd Win Byts","Fwd Seg Size Min","Init Fwd Win Byts",
+                      "Bwd Pkt Len Std","Fwd IAT Min"]
+}
 
 
 
+def analyze_save(csv_to_analyze,save_to):
+    df = pd.DataFrame()
+    test_dataframe = pd.read_csv(csv_to_analyze)
+    test_dataframe.drop_duplicates(subset=['Flow ID'],inplace=True)
+    df = test_dataframe[['Src IP', 'Src Port', 'Dst IP','Dst Port','Protocol',
+                        'Timestamp']].copy()
+    test_dataframe.drop(['Flow ID', 'Src IP', 'Src Port', 'Dst IP','Dst Port','Protocol',
+                        'Timestamp','Flow Byts/s', 'Flow Pkts/s','Label'],inplace=True,axis=1)
+    predictions= bclf.predict(bpca.transform(bscaler.transform(test_dataframe.values)))
 
-pid = os.fork()
-
-if pid > 0:
-   
-    cwd = os.getcwd()
-    DATA_PATH = os.path.join(cwd,"cicflowmeter/data/daily")
-    MODEL_PATH = os.path.join(cwd,"brain/models")
-    s_read_index = 0
-    print("Parent process:Binary classifier","pid:",os.getpid())
-    binaryclass_model = load(MODEL_PATH +'/binary.joblib')
+    df['B/A'] = predictions
+    for clf_feature,clf in zip(features,clf_s):
+        y_p = clf.predict(test_dataframe[features[clf_feature]].values)
+        df[clf_feature.replace("_feature","")] = predictions
     
-    while True:
-        if tfile:=glob.glob(DATA_PATH +"/nmap.pcap_Flow.csv"): 
-            df = pd.read_csv(tfile[0])
-            s_read_index = 0
-            temp = classifier.binaryclassifier(s_read_index,df.copy(),binaryclass_model)
-            s_read_index += temp
-            break
-        else:
-            print("not found")
-            sleep(5)
-        
-
-
-else:
-    
-    cwd = os.getcwd()
-    DATA_PATH = os.path.join(cwd,"cicflowmeter/data/daily")
-    PR_DATA_PATH = os.path.join(cwd,"server/predicted_block.csv")
-    MODEL_PATH = os.path.join(cwd,"brain/models")
-    start_at_index = 0
-    print("Child process:Multi Class classifier","pid:",os.getpid())
-    multiclass_model = load(MODEL_PATH + '/mclass.joblib')
-
-    while True:
-
-        if pathlib.Path(PR_DATA_PATH).is_file():
-            try:
-                predicted_block = pd.read_csv(PR_DATA_PATH,index_col=False)
-                total_read = classifier.multiclassclassifier(start_at_index,predicted_block.copy(),multiclass_model)
-                start_at_index += total_read
-                break
-            except Exception as e:
-                print(e)
-                sleep(1)
-
-           
-     
-   
-    
-
-
+    for c,s,p,name in zip(clfs,scls,pcas,attack_names):
+        y_p = c.predict(p.transform(s.transform(test_dataframe.values)))
+        df[f'{name}PCA'] = y_p
+    df['avg'] = np.prod(df[['B/A','dos','ddos','portscan','patator','web','dosPCA','ddosPCA','portscanPCA','patatorPCA','webPCA']].values,axis=1)
+    df.to_csv(f'{save_to}/{csv_to_analyze.split("/")[::-1][0]}')
  
+
+while True:
+    
+    csv_to_analyze = f'{DATA_PATH}nmap.pcap_Flow.csv'
+
+    analyze_save(csv_to_analyze,save_to=output_dir)
+    break
+
+
 
 
 
