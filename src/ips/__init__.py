e83f7697a5133dfd0e70234685aa0b1f2f2bdff3
azwyane <debianbyte@gmail.com>
2022-04-19 14:57:33 +0545

    nftables executor to list and block

--- src/ips/__init__.py
+++ src/ips/__init__.py
@@ -1,1 +1,7 @@
 from . import iptb
+import nftables
+import json
+
+nft = nftables.Nftables()
+nft.set_json_output(True)
+rc,output,err = nft.cmd("add chain ip filter NGUARD")
--- src/ips/executor_new.py
+++ src/ips/executor_new.py
@@ -1,10 +1,8 @@
 import nftables
 import json
-import pprint
+
 nft = nftables.Nftables()
 nft.set_json_output(True)
-
-rc,output,err = nft.cmd("add chain ip filter NGUARD")
 
 #check for IPS host or network mode
 #if hostmode then forward all INPUT and OUTPUT traffic to NGUARD chain
@@ -18,12 +16,20 @@ def setup_networkmode_target():
     return
 
 
-def block(sip, sport, dip, dport,proto,iface,config:dict):
-    #read config for calling setup mode 
-    #read config and use policies to block port ip and network mentioned in config 
-    #run nft block cmd
-    pass
+def block(sip, sport, dip, dport,proto,iface,block_port_ip_network:str):
+    if block_port_ip_network == 'port':
+        rc,output,err = nft.cmd(f"add rule ip filter NGUARD tcp dport {dport} counter drop")
+        return err
+        
+    elif block_port_ip_network == 'ip-port':
+        rc,output,err = nft.cmd(f"add rule ip filter NGUARD ip saddr {sip} tcp dport {dport} counter drop")
+        return err
 
+    elif block_port_ip_network == 'ip':
+        rc,output,err = nft.cmd(f"add rule ip filter NGUARD ip saddr {sip} tcp counter drop")
+        return err  
+
+    
 def unblock(sip, sport, dip, dport,proto,iface,config:dict):
     #run nft cmf to remove entry from the NGUARD chain
     pass
@@ -32,10 +38,18 @@ def unblock(sip, sport, dip, dport,proto,iface,config:dict):
 
 def list_rules():
     rc,output,err = nft.cmd("list ruleset")
-    return json.loads(output)
-    # rules = output['nft']['rule']
-    # nguard_rules = [k,v if  ]
+    table_chain_rule = json.loads(output)['nftables']
+    nguard_rules= []
+    for tcr in table_chain_rule:
+        if tcr.get('rule',None):
+            if tcr['rule']['chain'] == 'NGUARD':
+                nguard_rules.append(rule)
+    return {'nguard_rules':nguard_rules}
+]
 
+def flush_NGUARD_chain():
+    rc,output,err = nft.cmd("flush chain ip filter NGUARD")
+    return rc,err #if rc!= means error running json load error
 
 
 
